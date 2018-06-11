c885a37a6541a6ca5b868287d2cc1a966390354a
zhudeming <zhudeming620@aliyun.com>
2018-06-11 12:07:46 +0800

    zuul

---
+++ src/main/java/com/neo/ZuulApplication.java
@@ -0,0 +1,22 @@
+package com.neo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class ZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApplication.class, args);
+	}
+
+//	@Bean
+//	public TokenFilter tokenFilter() {
+//		return new TokenFilter();
+//	}
+
+}
